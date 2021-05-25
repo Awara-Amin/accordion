420651f043756bdee9ed3cee22291c23806d9564
AaA <67450869+Awara-Amin@users.noreply.github.com>
2021-05-25 12:21:12 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import data from './data';
+import SingleQuestion from './Question';
+
+
+
+
+
+function App() {
+  const [questions,setQuestions ] = useState(data);
+
+  return (
+    <main>
+    <div className='container'>
+      <h3>
+        question and answers about login
+      </h3>
+      <section className='info'>
+        {questions.map((question) => {
+          return <SingleQuestion key={question.id} {...question} />;
+        })}
+      </section>
+    </div>
+  </main>
+);
+  // <h2>accordion project setup</h2>;
+}
+
+export default App;
